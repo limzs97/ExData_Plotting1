2c4aee3a8e008f340b399a9eea17fa6ccba1e12d
limzs97 <limzs2001@gmail.com>
2020-12-18 15:12:12 +0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -1,4 +1,7 @@
-hpc_tbl <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+hpc_tbl <- read.table(unz(temp, "household_power_consumption.txt"), header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+unlink(temp)
 
 hpc_tbl$Date <- as.Date(hpc_tbl$Date, "%d/%m/%Y")
 
